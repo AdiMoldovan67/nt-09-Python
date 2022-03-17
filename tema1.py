2a9344f098ea736ddde8f86d9b3672544120b131
Adi Moldovan <adimoldo@yahoo.com>
2022-03-17 23:56:09 +0200

    Tema 1

---
+++ tema1.py
@@ -0,0 +1,16 @@
+my_list=[7,8,9,2,3,1,4,10,5,6]
+print('my_list is',my_list)
+my_sorted_list=sorted(my_list)
+print(my_sorted_list)
+print(sorted(my_list,reverse=True))
+print(sorted(my_list)[1::2])
+print(sorted(my_list)[0::2])
+print(sorted(my_list)[2::3])
+
+
+
+
+
+
+
+
